8a723fea5d87e80f36ba220c98587c6b4a71859b
alondra mojica <mojicachavezalondramichelle@gmail.com>
2025-06-24 10:24:45 -0600

    Primer commit

---
+++ Projecto.java
@@ -0,0 +1,25 @@
+public class Projecto {
+    public static void main(String[] args) {
+        
+     String nombre = "estudiante";
+      int a = 5;
+      int b = 3;
+      int suma = a + b;
+
+      System.out.println("hola," + nombre + "!");
+      System.out.println("la suma de" + a + "+" + b + "es" + suma);
+      
+
+
+
+
+
+
+
+
+
+
+    }
+}
+    
+
