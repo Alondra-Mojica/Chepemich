ba0e375115110446926e4eb17b626e676faca003
alondra mojica <mojicachavezalondramichelle@gmail.com>
2025-06-24 10:30:39 -0600

    Tercer commit

--- Projecto.java
+++ Projecto.java
@@ -5,9 +5,12 @@ public class Projecto {
       int a = 10;
       int b = 3;
       int suma = a + b;
+      int mult = a * b;
 
       System.out.println("hola," + nombre + "!");
       System.out.println("la suma de" + a + "+" + b + "es" + suma);
+      System.out.println("La multiplicacion de " + a + "+" + b  +" es: " + mult);
+      
       
 
 
